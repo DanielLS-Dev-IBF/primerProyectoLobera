ea8be43deeeabcb864e5c28d3eb0251e4fc1d65b
K4ze <dlobsi5@gmail.com>
2024-09-17 17:04:58 +0200

    .\primerEntornoLobera.php

---
+++ primerProyectoLobera.php
@@ -0,0 +1,6 @@
+<?php
+
+ echo "<h5 style='font-size: 30px; padding: 20px;'>Lo has conseguido, bienvenido a tu segundo virtual site</h5>";
+ echo "<img src='mono.jpg' style='padding: 20px;'>";
+
+?>
